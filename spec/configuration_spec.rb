a092c3f6b96088ebeb99d72190eb0e85f7ba3bf8
namusyaka <namusyaka@gmail.com>
2014-05-21 09:56:02 +0900

    Add configuration test cases

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 
 describe Hensel::Configuration do
+  let(:configuration){ Hensel::Configuration.new }
+  let(:variables){ configuration.instance_variables.map{|v| v[1..-1] } }
   describe ".attr_boolean_accessor" do
-    let(:configuration){ Hensel::Configuration.new }
     it "can define an accessor and boolean methods" do
       expect(configuration.respond_to?(:sample)).to be_false
       Hensel::Configuration.attr_boolean_accessor :sample
@@ -11,8 +12,20 @@ describe Hensel::Configuration do
   end
 
   describe "#[]" do
+    it "can get value of instance variables" do
+      variables.each.with_index do |variable, index|
+        configuration.send("#{variable}=", index)
+        expect(configuration[variable]).to eq(index)
+      end
+    end
   end
 
   describe "#[]=" do
+    it "can set value to instance variables" do
+      variables.each.with_index do |variable, index|
+        configuration.send(:[]=, variable, index)
+        expect(configuration[variable]).to eq(index)
+      end
+    end
   end
 end
